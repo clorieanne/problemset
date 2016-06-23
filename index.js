54804883da63c4fb746850710ee4667d88330a66
clorieanne <clorieanne@gmail.com>
2016-06-23 12:22:41 +0800

    feat(doAsync): Solution for problem number 1, Asynchronous Operations

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // solutions here
+var async = require('async');
+var lib = require('./lib/lib.js');
+
+
+function doAsync (arr, callback) {
+	for (var i = 0; i < arr.length; i++) {
+		element = arr[i];
+		if (typeof element === 'object'){
+			async.parallel({
+			    one: function(callback, results){
+			        doAsync(element);
+			        //console.log(element);
+			    },
+			});
+		}else{
+			lib.asyncOp(element);
+		}
+	};
+}
+
+let input = [
+  'A',
+  [ 'B', 'C' ],
+  'D'
+]
+
+doAsync(input);
